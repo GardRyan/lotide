fc883ba352ba003fd54437603d578296fb0b909f
GardRyan <res.gardner@gmail.com>
2024-01-09 16:22:34 -0500

    added emojis

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,21 @@
 const assertEqual = function(actual, expected) {
 
-  if (actual === Number(actual) && expected === Number(expected)) {
-    return actual + expected;
-
+  if (actual === expected) {
+    console.log("❤️❤️❤️Assertion Passed: " + $(actual) + "=" + $(expected));
+  }else {
+    console.log("😔😔😔Assertion Failed: " + $(actual) + "!==" + $(expected));
+  }
+}
 
 
 
 //test code
 
-assertEqual("Lighthouse Labs", "Bootcamp");
+
 assertEqual(1,1);
 
 // we want to code to be two of the same objects
 //two strings
-//two numbers
+//two numbers 
 
 //if yes, return result. If no, return error message.
