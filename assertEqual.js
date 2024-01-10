0625e8633dfc03c7e88938b48974b19021785ca5
GardRyan <res.gardner@gmail.com>
2024-01-10 12:59:41 -0500

    reneged updates. Reupload

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,11 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log("❤️❤️❤️Assertion Passed: " + $(actual) + "=" + $(expected));
-  }else {
-    console.log("😔😔😔Assertion Failed: " + $(actual) + "!==" + $(expected));
-  }
-}
-
+  const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`❤️❤️❤️Assertion Passed: ${actual} = ${expected}`);
+    }else {
+      console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
+    }
+    
+    }
 
 
 //test code
