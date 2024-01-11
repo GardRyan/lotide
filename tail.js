2271515095728bab4100b7303ddff2775eff6551
GardRyan <res.gardner@gmail.com>
2024-01-11 10:13:15 -0500

    final!

--- tail.js
+++ tail.js
@@ -1,36 +1,6 @@
-/*function tail(arr) {
-  for (things of arr) {
-   if (arr = arr[0]) {
-    return null;
-   }
-  return arr;
-  }
-  }
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`❤️❤️❤️Assertion Passed: ${actual} = ${expected}`);
-  }else {
-    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-  }
-  
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-console.log(result);
-
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-*/
-
-// Function to compare two parameters const assertEqual = (actual, expected) => { // if both parameters are equal to one another it will pass if (actual === expected) { console.log(🟢🟢🟢 Assertion Passed: ${actual} === ${expected}); // else it will print fail } else { console.log(🛑🛑🛑 Assertion failed: ${actual} !== ${expected}); } };
+// create a new array, remove first value
+// assertEqual new array
+// ensure that old array is the same
 
 const tail = (arr) => { 
   let newArr = []; 
@@ -40,9 +10,16 @@ const tail = (arr) => {
   return newArr;
  };
 
-// Test Case: Check the original array const words = ["Yo Yo", "Lighthouse", "Labs"]; tail(words); // no need to capture the return value since we are not checking it assertEqual(words.length, 3); // original array should still have 3 elements!
+ const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️❤️❤️Assertion Passed: ${actual} = ${expected}`);
+  }else {
+    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+  }
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); 
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
