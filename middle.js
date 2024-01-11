4ece900593e93c6ac9661738e087c219ab128ad7
GardRyan <res.gardner@gmail.com>
2024-01-11 10:06:25 -0500

    relinked

---
+++ middle.js
@@ -0,0 +1,15 @@
+// find the middle of the array
+// return those results
+//
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️❤️❤️Assertion Passed: ${actual} = ${expected}`);
+  }else {
+    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+  }
