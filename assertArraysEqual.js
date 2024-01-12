65f98cfb1ccdf587c4d047299c331d7fbc0d9b12
GardRyan <res.gardner@gmail.com>
2024-01-11 22:08:59 -0500

    adjusted for paramters of problem

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,21 @@
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
-const eqArrays = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1.length !== arr2.length) {
-      return false;
-      //console.log("Array's are different lengths!")
-    }else if (arr1[i] !== arr2[i]) {
-      //console.log("Arrays are not the same");
-      return false;
+      
+      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are equal!");
+      //return false;
+    } else if (arr1[i] !== arr2[i]) {
+      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are equal!");
+      //return false;
     }
   }
 
-  //console.log("Arrays are the same");
-  return true;
-}
+  
+  console.log("❤️❤️❤️Arrays " + arr1 + " and " + arr2 + " are equal!");
+  //return true;
+};
 
-const assertArraysEqual = function() {
-  if (eqArrays(arr1, arr2)) {
-  console.log("Arrays are  equal!");
-  }else {
-  console.log("Arrays are not equal!");
-}
-}
+
+assertArraysEqual([1,2,3], [1,2,3]);
