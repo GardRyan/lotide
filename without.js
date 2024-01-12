4d810fb2b414cf4b09d14112a9a79b83e9f59b7d
GardRyan <res.gardner@gmail.com>
2024-01-12 16:39:07 -0500

    overwritten. reupload

--- without.js
+++ without.js
@@ -1,39 +1,39 @@
 //Implement without which will return a subset of a given array, removing unwanted elements.
 
 // This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array.
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1.length !== arr2.length) {
-      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are not equal!")
+      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are not equal!");
       return;
-    }else if (arr1[i] !== arr2[i]) {
-      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are not equal!")
+    } else if (arr1[i] !== arr2[i]) {
+      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are not equal!");
       return;
     }
   }
 
   
-  console.log("❤️❤️❤️Arrays " + arr1 + " and " + arr2 + " are equal!")
+  console.log("❤️❤️❤️Arrays " + arr1 + " and " + arr2 + " are equal!");
   //return true;
-}
+};
 
 
-    const without(arr1, arr2) => {
-      let resultArr = []
+const without = (arr1, arr2) => {
+  let resultArr = [];
       
-      for (let i = 0; i < arr1.length; i++) {
-        if (!arr2.includes(arr1[i])) {
-          resultArr.push(arr1[i]);
-        }
-      }
-      console.log(resultArr);
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      resultArr.push(arr1[i]);
     }
+  }
+  console.log(resultArr);
+};
 
-without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
-//Make sure the original array was not altered by the without function 
+//Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
