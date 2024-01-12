2493787b6baf0a33eb4efbaaea2873162ea2a9b8
GardRyan <res.gardner@gmail.com>
2024-01-12 16:12:08 -0500

    func without

--- without.js
+++ without.js
@@ -2,39 +2,21 @@
 
 // This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array.
 
-// const assertArraysEqual = function (arr1, arr2) {
-//     for (let i = 0; i < arr1.length; i++) {
-//       if (arr1.length !== arr2.length) {
-//         console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are not equal!")
-//         return;
-//       }else if (arr1[i] !== arr2[i]) {
-//         console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are not equal!")
-//         return;
-//       }
-//     }
-
 let resultArr = []
 
-    function eqArrays(arr1, arr2) {
+    function without(arr1, arr2) {
       for (let i = 0; i < arr1.length; i++) {
         if (arr1[i] !== arr2[i]) {
-          return arr1.slice(arr2[i]);
+          resultArr.push(arr2[i]);
         }
       }
-    
-    //   console.log("Arrays are the same");
-    //   return true;
-    // }
-  
-    
-  //    console.log("❤️❤️❤️Arrays " + arr1 + " and " + arr2 + " are equal!")
-  //    //return true;
-  //  }
-  
+      console.log(resultArr);
+    }
 
 without([1, 2, 3], [1]) // => [2, 3]
 without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
 const words = ["hello", "world", "lighthouse"];
-//without(words, ["lighthouse"]); // no need to capture return value for this test case
-//Make sure the original array was not altered by the without function
-//assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
