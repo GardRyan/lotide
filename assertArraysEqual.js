aa11f0b7a8f704b5326baf79c240bddf330163c9
GardRyan <res.gardner@gmail.com>
2024-01-11 22:12:51 -0500

    last edits

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,24 @@
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
-const assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = function (arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1.length !== arr2.length) {
       
-      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are equal!");
-      //return false;
-    } else if (arr1[i] !== arr2[i]) {
-      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are equal!");
-      //return false;
+      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are not equal!")
+      return;
+    }else if (arr1[i] !== arr2[i]) {
+      console.log("😔😔😔Arrays " + arr1 + " and " + arr2 + " are not equal!")
+      return;
     }
   }
 
   
-  console.log("❤️❤️❤️Arrays " + arr1 + " and " + arr2 + " are equal!");
+  console.log("❤️❤️❤️Arrays " + arr1 + " and " + arr2 + " are equal!")
   //return true;
-};
+}
 
 
 assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual([1,3], [1,2,3]);
+assertArraysEqual([1,2,6], [1,2,3]);
+assertArraysEqual([1,3], [2,3]);
