4e2bb786d415d9a99ee1ad40f0002a230ad1574e
GardRyan <154691376+GardRyan@users.noreply.github.com>
2024-01-11 22:00:46 -0500

    Update assertArraysEqual.js
    local was unable to push changes to github

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,11 @@
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`❤️❤️❤️Assertion Passed: ${actual} = ${expected}`);
-//   } else {
-//     console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
-//   }
-  
-// };
-
-function eqArrays(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1.length !== arr2.length) {
       return false;
       //console.log("Array's are different lengths!")
-    } else if (arr1[i] !== arr2[i]) {
+    }else if (arr1[i] !== arr2[i]) {
       //console.log("Arrays are not the same");
       return false;
     }
@@ -24,10 +15,10 @@ function eqArrays(arr1, arr2) {
   return true;
 }
 
-function assertArraysEqual() {
-  if (eqArrays() === false) {
-    console.log("Arrays are not equal!");
-  }
-  console.log("Arrays are equal!");
+const assertArraysEqual = function() {
+  if (eqArrays(arr1, arr2)) {
+  console.log("Arrays are  equal!");
+  }else {
+  console.log("Arrays are not equal!");
 }
-
+}
