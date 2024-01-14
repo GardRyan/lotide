95375c7db8027790144718579d75bf87c952bffa
GardRyan <res.gardner@gmail.com>
2024-01-13 19:28:10 -0500

    function works. eslint

---
+++ findKey.js
@@ -0,0 +1,35 @@
+// assertEqual
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️❤️❤️Assertion Passed: ${actual} = ${expected}`);
+  } else {
+    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+
+// findKey Function
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+//test
+let result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+
+assertEqual(result, "noma");
