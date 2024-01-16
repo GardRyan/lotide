4ca40da809aed493f6277ce293d44e75ad704ed3
GardRyan <res.gardner@gmail.com>
2024-01-15 22:56:04 -0500

    refactor

--- tail.js
+++ tail.js
@@ -1,6 +1,7 @@
 // create a new array, remove first value
 // assertEqual new array
 // ensure that old array is the same
+const assertEqual = require('./assertEqual');
 
 const tail = (arr) => { 
   let newArr = []; 
@@ -10,16 +11,10 @@ const tail = (arr) => {
   return newArr;
  };
 
- const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`❤️❤️❤️Assertion Passed: ${actual} = ${expected}`);
-  }else {
-    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-  }
 
 // Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail;
